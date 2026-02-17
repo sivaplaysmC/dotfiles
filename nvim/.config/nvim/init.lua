702e1361b9972655bee9e9ecef26b02348a0910e
ishmael <ssepiol42@gmail.com>
2026-02-18 02:04:04 +0530

    nvim: treesitter: update some stuffs

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -576,71 +576,14 @@ vim.api.nvim_create_autocmd("LspAttach", {
 })
 
 later(function()
-	---@diagnostic disable-next-line: missing-fields
-	require("nvim-treesitter").setup({
-		-- Add languages to be installed here that you want installed for treesitter
-		sync_install = false,
-		ignore_install = {},
-		ensure_installed = { "c", "cpp", "go", "lua", "python", "rust", "tsx", "typescript", "vimdoc", "vim" },
-
-		-- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-		auto_install = true,
-
-		highlight = { enable = true },
-		indent = { enable = true },
-		incremental_selection = {
-			enable = true,
-			keymaps = {
-				init_selection = "<c-space>",
-				node_incremental = "<c-space>",
-				scope_incremental = "<c-s>",
-				node_decremental = "<M-space>",
-			},
-		},
-		textobjects = {
-			select = {
-				enable = true,
-				lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-				keymaps = {
-					-- You can use the capture groups defined in textobjects.scm
-					["aa"] = "@parameter.outer",
-					["ia"] = "@parameter.inner",
-					["af"] = "@function.outer",
-					["if"] = "@function.inner",
-					["ac"] = "@class.outer",
-					["ic"] = "@class.inner",
-				},
-			},
-			move = {
-				enable = true,
-				set_jumps = true, -- whether to set jumps in the jumplist
-				goto_next_start = {
-					["]m"] = "@function.outer",
-					["]]"] = "@class.outer",
-				},
-				goto_next_end = {
-					["]M"] = "@function.outer",
-					["]["] = "@class.outer",
-				},
-				goto_previous_start = {
-					["[m"] = "@function.outer",
-					["[["] = "@class.outer",
-				},
-				goto_previous_end = {
-					["[M"] = "@function.outer",
-					["[]"] = "@class.outer",
-				},
-			},
-			swap = {
-				enable = true,
-				swap_next = {
-					["<leader>a"] = "@parameter.inner",
-				},
-				swap_previous = {
-					["<leader>A"] = "@parameter.inner",
-				},
-			},
-		},
+	local filetypes =
+		{ "bash", "c", "diff", "html", "lua", "luadoc", "markdown", "markdown_inline", "query", "vim", "vimdoc" }
+	-- require("nvim-treesitter").install(filetypes)
+	vim.api.nvim_create_autocmd("FileType", {
+		pattern = filetypes,
+		callback = function()
+			vim.treesitter.start()
+		end,
 	})
 end)
 
